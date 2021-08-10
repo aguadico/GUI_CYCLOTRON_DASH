b60a8a2ee08710964dda9c6b6d357bd686e5c7b2
anagtv <ana.garcia-tabares.valdivieso@cern.ch>
2021-08-10 18:36:19 +0200

    additional_functions contains all the functions needed for plotting: one file, trends, cumulative charge and the functions needed to perform the ion source fit (with and without vacuum)

--- additional_functions.py
+++ additional_functions.py
@@ -9,59 +9,76 @@ import ion_source_studies
 import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import cyclotron_class
 
 
-def plotting_simple(fig,x_values,y_values,y_values_error,units,position_x,position_y,colori):
-    fig.add_trace(go.Scatter(x=x_values, y=y_values,mode='markers',                                                         
+
+def plotting_simple_name(fig,x_values,y_values,y_values_error,units,position_x,position_y,colori,namei,reference_value):
+    fig.add_trace(go.Scatter(x=x_values, y=y_values,name=namei,mode='markers',                                                         
     marker=go.Marker(color=colori),
         error_y=dict(
             type='data',
             symmetric=True,
-            array=y_values_error,
+            array=y_values_error
             )),row=position_x, col=position_y)
-    fig.update_yaxes(title_text=units, row=position_x, col=position_y)
+    print ("REFERNCE VALUE")
+    print (reference_value)
+    for i in range(len(reference_value)):
+         print ("IN THE LOOP")
+         print (reference_value[i])
+         print (reference_value[i][0])
+         print (reference_value[i][1])
+         print (reference_value[i][2])
+         fig.add_hrect(y0=reference_value[i][0], y1=reference_value[i][1]*reference_value[i][0], line_width=0, fillcolor="red", opacity=0.05,row=position_x, col=position_y)
+         fig.add_hline(y=reference_value[i][0], line_dash="dot",line_color="red",annotation_text=reference_value[i][2],
+                 annotation_position="bottom right",row=position_x, col=position_y)
+    #fig.add_hline(y=reference_value, line_dash="dot",line_color="green",
+    #          annotation_text="Reference value", 
+    #          annotation_position="bottom right",row=position_x, col=position_y)
+    #fig.add_hline(y=np.average(y_values), line_dash="dot",line_color="blue",
+    #          annotation_text="Average", 
+    #          annotation_position="bottom right",row=position_x, col=position_y)
+    fig.update_yaxes(title_text=units,row=position_x, col=position_y)
     return fig
 
-def plotting_simple_no_error(fig,x_values,y_values,units,position_x,position_y,colori):
-    fig.add_trace(go.Scatter(x=x_values, y=y_values,mode='markers',                                                         
+def plotting_simple_no_error(fig,x_values,y_values,units,position_x,position_y,colori,namei):
+    fig.add_trace(go.Scatter(x=x_values, y=y_values,mode='markers',name=namei,                                                        
     marker=go.Marker(color=colori)),row=position_x, col=position_y)
     fig.update_yaxes(title_text=units, row=position_x, col=position_y)
     return fig
 
-#def plotting_three_variables(fig,x_values,y_values,y_values_error,names,units):
-#    fig = plotting_simple(fig,x_values,y_values[0],y_values_error[0],names[0],units[0],1,1)
-#    fig.add_trace(go.Scatter(x=x_values, y=y_values[1],mode='markers',                                                         
-#    marker=go.Marker(color=colori), error_y=dict(
-#            type='data',
-#            symmetric=True,
-#            array=y_values_error[1],
-#            ),name=names[1]),row=2, col=1)
-#    fig.add_trace(go.Scatter(x=x_values, y=y_values[2],mode='markers',
-#        error_y=dict(
-#            type='data',
-#            symmetric=True,
-#            array=y_values_error[2],
-#            ),name=names[2]),row=3, col=1)
-#    
-#    fig.update_yaxes(title_text=units[2], row=3, col=1)
-#    return fig
 
 def getting_information(cyclotron_information,target_1,target_2,list_of_contents, list_of_names, list_of_dates):
     for c, n, d in zip(list_of_contents, list_of_names, list_of_dates): 
         #all_names.append(str(n[:-4]))
         parse_contents(cyclotron_information,c, n, d) 
-        cyclotron_information.file_output()
-        if float(cyclotron_information.target_number) in [1.0,2.0,3.0]: 
-           print ("THIS TARGET")
-           target_1.selecting_data_to_plot_reset(cyclotron_information)
-        else:
-           print ("OR THIS ONE")
-           target_2.selecting_data_to_plot_reset(cyclotron_information)
+        target_current = cyclotron_information.file_df.Target_I.astype(float)
+        pre_irradiation_len = (len(cyclotron_information.file_df.Target_I[cyclotron_information.file_df['Target_I'].astype(float) == 50.0].astype(float))) + (len(cyclotron_information.file_df.Target_I[cyclotron_information.file_df['Target_I'].astype(float) == 25.0].astype(float))) + (len(cyclotron_information.file_df.Target_I[cyclotron_information.file_df['Target_I'].astype(float) == 0.0].astype(float)))
+        pre_irradiation_len_relative = (pre_irradiation_len/len(cyclotron_information.file_df.Target_I.astype(float)))
+        max_current = np.max(cyclotron_information.file_df.Target_I.astype(float))    
+        #if ((pre_irradiation_len_relative) < 0.3 and float(max_current) > 15):
+        if (float(max_current) > 15):
+            cyclotron_information.file_output()
+            if float(cyclotron_information.target_number) in [1.0,2.0,3.0]: 
+               print ("THIS TARGET")
+               target_1.selecting_data_to_plot_reset(cyclotron_information)
+            else:
+               print ("OR THIS ONE")
+               target_2.selecting_data_to_plot_reset(cyclotron_information)
     saving_trends_alt.getting_summary_final(cyclotron_information) 
     target_1.get_summation_per_period()
     target_2.get_summation_per_period()  
     cyclotron_information.source_performance = np.average(np.array(cyclotron_information.source_performance_total)[np.array(cyclotron_information.source_performance_total) > 0])         
-
+    df_target_1 = target_1.df_information_foil
+    df_target_2 = target_2.df_information_foil
+    cyclotron_information.get_average_std_summary_cummulative(df_target_1,df_target_2)
+    cyclotron_information.physical_targets = [np.min(cyclotron_information.df_extraction.PHYSICAL_TARGET),np.max(cyclotron_information.df_extraction.PHYSICAL_TARGET)]
+    print ("CYCLOTRON INFORMATION")
+    print (cyclotron_information.df_summary)
+    print ("CYCLOTRON INFORMATION (EXTRACTION)")
+    print (cyclotron_information.df_extraction)
+    print (cyclotron_information.df_extraction.CAROUSEL_POSITION_AVE)
+    print (cyclotron_information.df_magnet)
 
 def current_vaccum(X, a,b):
      x,y,z = X
